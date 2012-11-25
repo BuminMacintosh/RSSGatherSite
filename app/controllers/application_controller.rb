520b21f6df9c551ce3a22fafc60e04be3d49b149
BuminMacintosh <bumin_pol-software@yahoo.co.jp>
2012-11-25 23:30:01 +0900

    Test Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 ﻿class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :set_locale
-
-  def set_locale
-    I18n.locale = "ja" || I18n.default_locale
-  end
 end
