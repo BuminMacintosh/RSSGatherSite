6ef146f5e9253f581918bd2d4c5b9a09d7abac4b
BuminMacintosh <bumin_pol-software@yahoo.co.jp>
2012-11-25 23:01:56 +0900

    Version 0.3.2.1

--- config/environment.rb
+++ config/environment.rb
@@ -3,9 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 RSSGatherSite::Application.initialize!
-
-# Adding the below makes it work as expected at heroku
-Rails::Initializer.run do | config |
-  config.i18n.default_locale = :ja
-  config.i18n.locale = :ja
-end
